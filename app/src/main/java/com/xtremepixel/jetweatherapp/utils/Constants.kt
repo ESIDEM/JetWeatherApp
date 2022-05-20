168a68370bb41cfd7af5b6445cad865973403296
samesidem <samesidem@gmail.com>
2022-05-20 10:19:44 +0100

    Constants file removed

--- app/src/main/java/com/xtremepixel/jetweatherapp/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.xtremepixel.jetweatherapp.utils
-
-object Constants {
-
-    const val   BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "7380bad2bfcd760b765a87aef6d60ff2"
-    const val IMAGE_URL = "https://openweathermap.org/img/wn/"
-    const val IMAGE_EX = ".png"
-}
